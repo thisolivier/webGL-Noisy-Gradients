bb3dcdfffc4e6fe6733efa46922e09e7847f06e2
Olivier <olivier.butler@gmail.com>
2025-07-13 23:10:02 +0100

    Extended gradient

--- gradients.js
+++ gradients.js
@@ -1,16 +1,16 @@
 export const gradients = [
   {
-    xNorm: 0.2,
+    xNorm: 0.01,
     yNorm: 0,
     speed: 0.3,
-    radius: 0.1,
+    radius: 0.2,
     colour: [0.98, 0.42, 1.0], // pink in GLSL Vec3 format
   },
   {
-    xNorm: 0.2,
+    xNorm: 0,
     yNorm: 0,
     speed: 0.3,
-    radius: 0.05,
+    radius: 0.12,
     colour: [0.624, 0.220, 1.0], // pink in GLSL Vec3 format
   },
   {
@@ -26,6 +26,13 @@ export const gradients = [
     speed: 0.3,
     radius: 0.1,
     colour: [0.788, 1.0, 0.992] // light blue
-  }
+  },
+    {
+    xNorm: 0.2,
+    yNorm: 0.8,
+    speed: 0.6,
+    radius: 0.2,
+    colour: [1.0, 1.0, 0.0], // yellow
+  }, 
   // up to 16 entries
 ];
